00ffc536a5d42c8956c5ac0e78f60ae66cabf5bc
Kishor-khade <kishorkhade5105@gmail.com>
2022-07-10 19:54:48 +0530

    BlackJack version 2

--- Blackjack.py
+++ Blackjack.py
@@ -1,72 +1,69 @@
-# 
 import random
 
-
 def get_card():
+    """It return's one random card among 14 cards"""
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
-def print_fun(comp, user):
-    print("\ncomputer has ", comp)
-    print("you have ",user)
-    return
-
-def check(card):
-    if card >21:
-        return 0
-    elif card == 21:
-        return 1
+def calculate_score(list):
+    """calculate the score for the taken cards"""
+    if sum(list)==21 and len(list)==2:
+        return 21
+    elif sum(list)>21 and 11 in list:
+        list.remove(11)
+        list.append(1)
+        return sum(list)
     else :
-        return 2
+        return sum(list)
+        
+def compare(user, comp):
+    """It compare's both the total and declares the Result"""
+    if (user > 21) or (comp > user):
+        print("\n------You loose------")
+    elif (comp > 21) or (comp < user):
+        print("\n------You won------")
+    else:
+        print("\n------Match Draw------")
 
-def play(user, comp):
-    while (True):
-        score = check(user)
-        if score == 0:
-            print_fun(comp, user)
-            print("you got more than 21 \n ------You loose------")
-            return
-        if score == 1:
-            print_fun(comp, user)
-            print("you got 21 \n ------You Won------")
-            return
-        print("you have ", user)
-        choice = input(" If you want to take another one card enter 'y' otherwise enter 'n'")
-        if choice == 'y' or choice == 'Y':
-            user += get_card()
+
+def play():
+    user_cards = []
+    comp_cards = []
+
+    for _ in range(2):
+        user_cards.append(get_card())
+        comp_cards.append(get_card())
+
+    """User's Turn"""
+    while(True):
+        user_score = calculate_score(user_cards)
+        print (f"\nyou cards are {user_cards} , your score is {user_score}")
+        print (f"Computer's first card is {comp_cards[0]}")
+        val = input("\nif you want to add one card enter 'y' otherwise enter 'n' : ")
+        if val=='y' or val=='Y':
+            user_cards.append(get_card())
+            pass
         else:
             break
+
+    """Computer's Turn"""
+    while(True):
+        comp_score = calculate_score(comp_cards)
+        if comp_score < 17:
+            comp_cards.append(get_card())
+        else:
+            break
+
+    print(f"computer has {comp_cards},  And total score is {comp_score}")
+    print(f"You have {user_cards},  And total score is {user_score}")
+
+    compare(user_score, comp_score)
     
-    while (True):
-        if check(comp) == 0:
-            print_fun(comp, user)
-            print ("computer score is more than 21 \n ------You Won------")
-            return
-        if comp < 17: comp += get_card()
-        else : 
-            print_fun(comp, user)
-            if comp <= user:print("\n ------Computer loose------")
-            else: print("------Computer Won------")
-            return
-
-def start():
-    #1st round
-    user, comp = 0,0
-    comp += get_card()
-    user += get_card()
-    show = comp
-
-    #2nd round
-    user += get_card()
-    comp += get_card()
-    print_fun(show, user)
-    play(user, comp)
 
 while(True):
     inp = input("\nIf you want to play game enter 'y' ,otherwise enter 'n' : ")
-    if inp == 'y' or inp == 'Y':
-        start()
-        print("yes")
+    if (inp == 'y') or (inp == 'Y'):
+        play()
     else:
-        print("No")
         break
+    print("\nx-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x")
